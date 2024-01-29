8463c86c1ab2a0cc628b2e6ec52a11d9ca583305
sooohi <68193613+sooohi@users.noreply.github.com>
2024-01-29 16:56:48 +0900

    feat: 초기설정

---
+++ TrackUs/TrackUs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackUs
+//
+//  Created by 박소희 on 1/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackUs/TrackUs/TrackUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackUsApp.swift
+//  TrackUs
+//
+//  Created by 박소희 on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
