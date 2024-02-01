be7ee177704120cf0b16fafa99b4f4ffa038691b
Seokki-Kwon <seokki0706@gmail.com>
2024-02-01 21:12:26 +0900

    #3 add: 배경 터치시 keyboard 숨기는 기능 추가

--- TrackUs/TrackUs/Sources/Commons/Extensions/View+Keyboard.swift
+++ TrackUs/TrackUs/Sources/Commons/Extensions/View+Keyboard.swift
@@ -5,4 +5,12 @@
 //  Created by 석기권 on 2024/02/01.
 //
 
-import Foundation
+import SwiftUI
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
