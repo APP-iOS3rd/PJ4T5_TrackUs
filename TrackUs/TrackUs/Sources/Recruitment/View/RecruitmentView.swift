b5d6dfae04640ca8268ca9c0df4b31d35a255a89
PinkSoju <yjs4083@naver.com>
2024-02-01 20:13:46 +0900

    fix: MainTabView 수정

---
+++ TrackUs/TrackUs/Sources/Recruitment/View/RecruitmentView.swift
@@ -0,0 +1,18 @@
+//
+//  RecruitmentView.swift
+//  TrackUs
+//
+//  Created by 윤준성 on 2/1/24.
+//
+
+import SwiftUI
+
+struct RecruitmentView: View {
+    var body: some View {
+        Text("모집뷰 개발 중입니다.")
+    }
+}
+
+#Preview {
+    RecruitmentView()
+}
